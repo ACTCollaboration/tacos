a197a5a7fb33232a2a033006e87e82bb8712f4bc
AdriJD <adri.j.duivenvoorden@gmail.com>
2021-05-13 16:02:36 -0400

    outlines for bandpass and models

--- tacos/bandpass.py
+++ tacos/bandpass.py
@@ -1,6 +1,113 @@
+import numpy as np
 
-# A class that represents a bandpass
+from pixell import enmap
 
-# init takes bandpass as argument and stores it, perhaps prepares interpolation
+from compsep import units
 
-# Method that takes arrays of amplitudes, frequency and returns integrated value
+class BandPass():
+    '''
+    Bandpass instance contains normalized bandpass and conversion
+    factors.
+
+    Parameters
+    ----------
+    bandpass : (nfreq) array
+        Bandpass.
+    nu : (nfreq) array
+        Frequencies in Hz.
+    '''
+    def __init__(self, bandpass, nu):
+        
+        self.bandpass = bandpass / np.trapz(bandpass, x=nu)
+        self.nu = nu
+
+        # Can compute conv factor at init, right?
+        # Can also compute interpolation table at init.
+
+    def conv_fact_rj_to_cmb(self):
+
+        return units.convert_rj_to_cmb(self.bandpass, self.nu)
+
+    def compute_interpolation(self):
+        
+        # Given range of betas, compute interpolation lookup table.
+        # For single beta: 1d table, for two betas 2d table etc.
+        # But are you doing this for all sky components? Or one
+        # component at a time? Perhaps not here then.
+        
+        raise NotImplementedError()
+
+    def integrate_over_bandpass(self):
+        pass
+
+    @classmethod
+    def load_hfi_bandpass(cls, filename, band, psb_only=True,
+                          nu_sq_corr=True):
+        '''
+        Read Planck HFI bandpass file and return class instance.
+
+        Parameters
+        ----------
+        filename : str
+            Absolute path to file.
+        band : str
+            Planck HFI band, e.g. '143'.
+        psb_only : bool, optional
+            Load special bandpasses for polarization sensitive bolometers
+            if available.
+        nu_sq_corr : bool, optional
+            Preprocess bandpasses by applying nu ** 2 correction factor.
+
+        Returns
+        -------
+        hfi_bandpass : bandpass.BandPass instance
+        '''
+
+        with h5py.File(filename + '.hdf5', 'r') as f:
+            factors = f['factors'][()]
+            rule = f['rule'][()]
+            weights = f['weights'][()]
+            ells = f['ells_full'][()]
+            name = f['name'][()].decode("utf-8") 
+
+        return cls(factors, rule, weights, ells, name)
+
+def get_mixing_matrix(bandpasses, betas, dtype=np.float32):
+    '''
+    Return mixing matrix for given frequency bands and signal
+    components.
+
+    Parameters
+    ----------
+    bandpasses : (nj) array-like of BandPass objects
+        Bandpass for each frequency band.
+    betas : (ncomp) array or (ncomp, ny, nx) enmap.
+        Spectral indices per components.
+    dtype : type
+        Dtype for output.
+    
+    Returns
+    -------
+    mixing_mat : (nj, ncomp) array or (nj, ncomp, ny, nx) enmap
+        Mixing matrix.
+    '''
+
+    nj = len(bandpasses)
+    ncomp = betas.shape[0]
+
+    if isinstance(betas, enmap.ndmap):
+        mixing_mat = enmap.zeros((nj,) + betas.shape), wcs=betas.wcs)
+    else:
+        mixing_mat = np.zeros((nj, ncomp), dtype=dtype)
+
+    for jidx in range(nj):
+
+        u_conv = bandpasses[jidx].conv_fact_rj_to_cmb()
+        bandpass = bandpasses[jidx].bandpass
+        nu = bandpass.nu
+
+        # NOTE, betas need nu dependence, right?
+        mixing_mat[jidx] = units.integrate_over_bandpass(
+            betas, bandpass, nu)
+
+    return mixing_mat
--- tacos/models.py
+++ tacos/models.py
@@ -4,12 +4,40 @@
 
 # Less generic class that represents a diffuse component (CMB, synch, dust etc.)
 
-   # __init__ method that takes parameters ...
+   # __init__ method that takes parameters: reference frequency..
 
-   # __call__ method that takes array of frequencies and returns amplitudes
+   # __call__ method that takes array of frequencies and returns amplitudes (perhaps not needed)
 
+   # Amplitude attribute? A map.. Will be updated inplace by Gibbs sampler. I, Q, U for example.
+
+   # beta attribute, another map. Could be several parameters... beta_d and T for dust.. 
+
+   # Perhaps, linear parameter attribute: dict of arrays. self.lin_par['amp']
+   # and self.nonlin_par['beta_d'].
+
+   # I.e the non-linear part
+   # defined to be 1 at the reference frequency.
    
-# Class that represents a linear parameter (amplitudes)
-
-# Class that represets a non-linear parameter (spectral indices)
+   # Total amplitude as function of frequency (amp + beta(nu)).
    
+import numpy as np
+from abc import ABC, abstractmethod
+
+class DiffuseComponent(ABC):
+    
+    @abstractmethod
+    def compute_amplitude(nu):
+        
+        pass
+
+class Dust(DiffuseComponent):
+    
+    def __init__(self):
+        pass
+
+class NonLinPar(ABC):
+    
+    @abstractmethod
+    def __call__(self, nu):
+        
+        
