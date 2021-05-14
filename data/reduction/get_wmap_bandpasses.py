fixed bug in hfi script (starting at 2nd hdu instead of 3rd), changed convert_rj_to_cmb to use wmap convention, added dw_dt function to units, fixed bug in wmap script (channel coadding was incorrect)

data/reduction/get_wmap_bandpasses.py
@@ -44,11 +44,11 @@ for bidx, band in enumerate(bands):
             # Coadd the two channels, Eq. 45 in Jarosik 2003 astro-ph/0301164.
             chn1 = out[1]
             chn2 = out[2]
-            w_prime = units.db_dt(nu) * cs.clight() ** 2 / 2 / cs.kboltz()
-            coadd = np.divide(chn1, w_prime * chn1, where=chn1!=0)
-            coadd += np.divide(chn2, w_prime * chn2, where=chn2!=0)
-            coadd *= 0.5
-            
+
+            w_prime = units.dw_dt(nu)
+            coadd = 0.5 * (chn1 / np.sum(chn1 * w_prime))
+            coadd += 0.5 * (chn2 / np.sum(chn2 * w_prime))
+
             key = band + str(da) + str(rad)
             bandpasses[key] = {}
             bandpasses[key]['nu'] = nu

tacos/bandpass.py
@@ -2,8 +2,9 @@ import numpy as np
 import os 
 
 from pixell import enmap
+import h5py
 
-from compsep import units
+from tacos import units
 
 class BandPass():
     '''
@@ -21,13 +22,8 @@ class BandPass():
         
         self.bandpass = bandpass / np.trapz(bandpass, x=nu)
         self.nu = nu
-
-        # Can compute conv factor at init, right?
-        # Can also compute interpolation table at init.
-
-    def conv_fact_rj_to_cmb(self):
-
-        return units.convert_rj_to_cmb(self.bandpass, self.nu)
+        self.c_fact_rj_to_cmb = units.convert_rj_to_cmb(
+            self.bandpass, self.nu)
 
     def compute_interpolation(self):
         
@@ -175,7 +171,7 @@ class BandPass():
                 nda = 4
 
             # Use average of bandpasses for now.
-            for didx, da in enumerate(range(1 + nda)):
+            for didx, da in enumerate(range(1, 1 + nda)):
                 for rad in range(1, 3):
                     
                     bandname = band + str(da) + str(rad)
@@ -213,7 +209,7 @@ def get_mixing_matrix(bandpasses, betas, dtype=np.float32):
     ncomp = betas.shape[0]
 
     if isinstance(betas, enmap.ndmap):
-        mixing_mat = enmap.zeros((nj,) + betas.shape), wcs=betas.wcs)
+        mixing_mat = enmap.zeros((nj,) + betas.shape, wcs=betas.wcs)
     else:
         mixing_mat = np.zeros((nj, ncomp), dtype=dtype)
 

tacos/units.py
@@ -28,9 +28,11 @@ def convert_rj_to_cmb(bandpass, nu):
     bandpass = np.atleast_1d(bandpass)
 
     # Note, typo in beyondplanck 2011.05609: eq. 39 uses h instead of kb.
-    numerator = np.trapz(2 * cs.kboltz() * (nu  / cs.clight()) ** 2 * bandpass,
-                         x=nu)
-    denominator = np.trapz(db_dt(nu) * bandpass, x=nu)
+    # We use Eq. 46 in Jarosik 2003 astro-ph/0301164.
+
+    numerator = np.trapz(bandpass, x=nu)
+    w_prime = dw_dt(nu)
+    denominator = np.trapz(w_prime * bandpass, x=nu)
 
     return numerator / denominator
 
@@ -61,10 +63,14 @@ def integrate_over_bandpass(signal, bandpass, nu, axis=-1):
 
     return np.trapz(signal * bandpass, x=nu, axis=axis)
 
-def db_dt(nu, temp=None):
+def dw_dt(nu, temp=None):
     '''
-    Return derivative of blackbody function with respect to temperature,
-    evaluated at a given temperature.
+    Return derivative of w function (defined in Eq. 38 in Jarosik 2003 
+    astro-ph/0301164) with respect to a given temperature. 
+
+    w = h nu  / (e^x - 1),    x = h nu / kB / T.
+    
+    dw / dT = x^2 e^x / (e^x - 1)^2.
 
     Arguments
     ---------
@@ -76,9 +82,9 @@ def db_dt(nu, temp=None):
 
     Returns
     -------
-    db_dt : (nfreq) array or float
-        Derivative of blackbody function with respect to temperature,                                              
-        evaluated at input temperature. In units of W / (sr m^2 Hz K).
+    dw_dt : (nfreq) array or float
+        Derivative of w respect to temperature, evaluated at input 
+        temperature (dimensionless).
     '''
 
     if temp is None:
@@ -114,9 +120,35 @@ def db_dt(nu, temp=None):
     # Works because lim_nu->inf nu^2 x^2 e^x / (e^x - 1)^2 = 0.
     out[mask_large] = 0
 
-    out *= 2 * kboltz * nu ** 2 / clight ** 2
-
     if ndim_in == 0:
         return out[0]
     else:
         return out
+    
+def db_dt(nu, temp=None):
+    '''
+    Return derivative of blackbody function with respect to temperature,
+    evaluated at a given temperature.
+
+    Arguments
+    ---------
+    nu : (nfreq) array or float
+        Monotonically increasing array of frequencies in Hz.
+    cmb_temp : float, optional
+        Evaluate derivate at this temperature in Kelvin, defaults
+        to CMB temperature.
+
+    Returns
+    -------
+    db_dt : (nfreq) array or float
+        Derivative of blackbody function with respect to temperature,                                              
+        evaluated at input temperature. In units of W / (sr m^2 Hz K).
+    '''
+
+    kboltz = cs.kboltz()
+    clight = cs.clight()
+    out = dw_dt(nu, temp=temp)
+
+    out *= 2 * kboltz * nu ** 2 / clight ** 2
+
+    return out

tests/test_units.py
@@ -56,8 +56,12 @@ class TestUnits(unittest.TestCase):
         nu = np.linspace(1e9, 410e9, num=10000)
         bandpass = np.ones_like(nu)
 
+        ##
+        #bandpass *= nu ** 2
+        #ba
+
         u_fact = units.convert_rj_to_cmb(bandpass, nu)
-        u_fact_exp = 5.6272697337624376 # From Mathematica.
+        u_fact_exp = 2.2348415733572202 # From Mathematica.
 
         self.assertAlmostEqual(u_fact, u_fact_exp, places=6)
 