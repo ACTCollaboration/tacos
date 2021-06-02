4d4de204f2be713287ea1925abe7185393885dd2
Zachary Atkins <zatkins2@gmail.com>
2021-06-02 02:36:18 -0400

    remove redundant reduction folder in tacos/data

--- tacos/data.py
+++ tacos/data.py
@@ -9,10 +9,9 @@ import numpy as np
 import yaml
 
 from soapack import interfaces as sints
+from pixell import enmap
 from tacos import utils
-
-import os
-import pkg_resources
+from tacos.bandpass import BandPass
 
 # copied from soapack.interfaces
 def config_from_yaml(filename):
@@ -23,10 +22,6 @@ def config_from_yaml(filename):
 # load config to data products
 config = sints.dconfig['tacos']
 
-# class dataset
-# class that loads a "consistent" dataset, meaning pixelization always and possibly a common beam
-# returns a dataset object, whose main purpose is to hold a dict of channel objects
-
 # this is the main file format, which handles all inputs and outputs
 ext_dict = {
     'map': 'fits',
@@ -35,7 +30,9 @@ ext_dict = {
     'beam': 'hdf5',
     }
 
-def data_str(type=None, instr=None, band=None, id=None, set=None, notes=''):
+def data_str(type=None, instr=None, band=None, id=None, set=None, notes=None):
+    if notes is None:
+        notes = ''
     data_str_template = '{type}_{instr}_{band}_{id}_{set}{notes}.{ext}'
     return data_str_template.format(
         type=type, instr=instr, band=band, id=id, set=set, notes=notes, ext=ext_dict[type]
@@ -45,14 +42,16 @@ def data_str(type=None, instr=None, band=None, id=None, set=None, notes=''):
 # and one frequency (and optionally, one detector subset)
 class Channel:
 
-    def __init__(self, instrument=None, band=None, id=None, correlated_noise=False):
+    def __init__(self, instrument=None, band=None, id=None, correlated_noise=False, notes=None):
         
+        # assign and store metadata
         self._instrument = instrument
         self._band = band
         if id is None:
             id = 'all'
         self._id = id
         self._correlated_noise = correlated_noise
+        self._notes = notes
 
         @property
         def instrument(self):
@@ -68,6 +67,32 @@ class Channel:
 
         @property
         def correlated_noise(self):
+            if self._correlated_noise:
+                raise NotImplementedError('Correlated noise not yet implemented')
             return self._correlated_noise
+
+        @property
+        def notes(self):
+            return self._notes
+
+        # get and store data
+        if self.correlated_noise:
+            pass
+        else:
+            covmat_type = 'icovar'
+            set = 'coadd'
+
+        map_path = config['maps_path'] + f'{instrument}/'
+        map_path += data_str(type='map', instr=instrument, band=band, id=id, set=set, notes=notes)
+        self._map = enmap.read_map(map_path)
+
+        covmat_path = config['covmats_path'] + f'{instrument}/'
+        covmat_path += data_str(type=covmat_type, instr=instrument, band=band, id=id, set=set, notes=notes)
+        self._covmat = enmap.read_map(covmat_path)
+
+        bandpass_path = config['bandpasses_path'] + f'{instrument}/'
+        bandpass_path += data_str(type='bandpass', instr=instrument, band=band, id=id, set=set, notes=notes)
+
+        
 
         
